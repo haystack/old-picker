75d0f67a76560be82f1303f0dbc489f96d0e74ea
leibovic <leibovic>
2007-08-15 19:26:08 +0000

    Updating unit-adder for sections. Alsoe changing format.

--- src/webapp/unit-adder.js
+++ src/webapp/unit-adder.js
@@ -3,28 +3,33 @@
  */
  
 function enableUnitAdder() {
- 	var collection = window.exhibit.getCollection("picked-classes");
+ 	var collection = window.exhibit.getCollection("picked-sections");
  	collection.addListener({ onItemsChanged: function() { 
-			var classes = collection.getRestrictedItems();
-			var currentSize = classes.size();
+			var sections = collection.getRestrictedItems();
+			var currentSize = sections.size();
 			if (currentSize > 0) {
-				var reportedHours = 0;
-				var expr = Exhibit.ExpressionParser.parse("add(!class.@reported-hours)");
-				expr.evaluate({ value: collection.getRestrictedItems() }, { value: "item" }, "value", window.database).values.visit(function(v) { reportedHours = v; } );				
-				var units = { lecture: 0, lab: 0, prep: 0 };
-				classes.visit(function(classID) {
-					var classUnits = database.getObject(classID, "units").split('-');
-					units.lecture = units.lecture + parseInt(classUnits[0]);
-					units.lab = units.lab + parseInt(classUnits[1]);
-					units.prep = units.prep + parseInt(classUnits[2]);
+				var reported = { hours: 0, courses: 0 };
+				sections.visit(function(sectionID) {
+					var hours = database.getObject(sectionID, "reported-hours");
+					reported.hours = reported.hours + hours;
+					if (hours > 0) { reported.courses = reported.courses + 1; }
 				});
-				var unitsString = units.lecture+"-"+units.lab+"-"+units.prep;
-				var unitsTotal = units.lecture + units.lab + units.prep;
+				
+				var units = { lecture: 0, lab: 0, prep: 0, total: 0, string: "" };
+				sections.visit(function(sectionID) {
+					var classID = database.getSubject(sectionID, "class");
+					var unit = database.getObject(classID, "unit");
+					units.lecture = units.lecture + parseInt(unit[0]);
+					units.lab = units.lab + parseInt(unit[1]);
+					units.prep = units.prep + parseInt(unit[2]);
+				});
+				units.string = units.lecture+"-"+units.lab+"-"+units.prep;
+				units.total = units.lecture + units.lab + units.prep;
 				
 				var div = document.getElementById('total-units');
-				div.innerHTML = 'Units: '+unitsString+
-								'<br>Total Units: '+unitsTotal+
-								'<br>Reported Total Hours: '+reportedHours+'<br>';
+				div.innerHTML = 'Total Units: '+units.string+' ('+units.total+')'+
+								'<br>Student Reported Total Hours: '+reported.hours+' ('+
+								reported.courses+' Courses with Data)<br>';
 								
 			    document.getElementById('no-picked-classes').style.display = "none";
 			} else {
