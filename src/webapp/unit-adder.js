2c501d79776670f126e64967ed7eff949eec192e
leibovic <leibovic>
2007-08-15 20:23:15 +0000

    Fixed unit-adder

--- src/webapp/unit-adder.js
+++ src/webapp/unit-adder.js
@@ -11,14 +11,17 @@ function enableUnitAdder() {
 				var reported = { hours: 0, courses: 0 };
 				sections.visit(function(sectionID) {
 					var hours = database.getObject(sectionID, "reported-hours");
-					reported.hours = reported.hours + hours;
-					if (hours > 0) { reported.courses = reported.courses + 1; }
+					if (hours !== null) {
+						reported.hours = reported.hours + (Math.ceil(10 * parseFloat(hours)) / 10);
+				   		reported.courses = reported.courses + 1; 
+				   	}
 				});
+				if (reported.courses == 1) { var courseWord = ' Course'; } else { var courseWord = ' Courses'; }
 				
 				var units = { lecture: 0, lab: 0, prep: 0, total: 0, string: "" };
 				sections.visit(function(sectionID) {
-					var classID = database.getSubject(sectionID, "class");
-					var unit = database.getObject(classID, "unit");
+					var classID = database.getObject(sectionID, "class");
+					var unit = database.getObject(classID, "units").split('-');
 					units.lecture = units.lecture + parseInt(unit[0]);
 					units.lab = units.lab + parseInt(unit[1]);
 					units.prep = units.prep + parseInt(unit[2]);
@@ -28,8 +31,8 @@ function enableUnitAdder() {
 				
 				var div = document.getElementById('total-units');
 				div.innerHTML = 'Total Units: '+units.string+' ('+units.total+')'+
-								'<br>Student Reported Total Hours: '+reported.hours+' ('+
-								reported.courses+' Courses with Data)<br>';
+								'<br>Reported Hours: '+reported.hours+' ('+
+								reported.courses+courseWord+')<br>';
 								
 			    document.getElementById('no-picked-classes').style.display = "none";
 			} else {
