f868b88f429e19628dd776250ea5f3dd303e261e
leibovic <leibovic>
2007-08-21 14:28:22 +0000

    Added STS data

---
+++ src/webapp/data/sts-lectures.js
@@ -0,0 +1,359 @@
+{
+	"items" :      [
+		{
+			"room" :    "E15-235",
+			"start" :   "13:00",
+			"id" :      "L-STS.445J-T13:00",
+			"uri" :     "http://127.0.0.1/L-STS.445J-T13%3A00",
+			"end" :     "15:00",
+			"type" :    "Lecture",
+			"day" :     "T",
+			"label" :   "L-STS.445J-T13:00",
+			"section" : "sSTS.445Ja"
+		},
+		{
+			"room" :    "4-231",
+			"start" :   "14:00",
+			"id" :      "L-STS.487-R14:00",
+			"uri" :     "http://127.0.0.1/L-STS.487-R14%3A00",
+			"end" :     "17:00",
+			"type" :    "Lecture",
+			"day" :     "R",
+			"label" :   "L-STS.487-R14:00",
+			"section" : "sSTS.487a"
+		},
+		{
+			"room" :    "1-371",
+			"start" :   "14:30",
+			"id" :      "L-STS.046J-T14:30",
+			"uri" :     "http://127.0.0.1/L-STS.046J-T14%3A30",
+			"end" :     "16:00",
+			"type" :    "Lecture",
+			"day" :     "T",
+			"label" :   "L-STS.046J-T14:30",
+			"section" : "sSTS.046Ja"
+		},
+		{
+			"room" :    "54-100",
+			"start" :   "10:00",
+			"id" :      "L-STS.005-T10:00",
+			"uri" :     "http://127.0.0.1/L-STS.005-T10%3A00",
+			"end" :     "11:00",
+			"type" :    "Lecture",
+			"day" :     "T",
+			"label" :   "L-STS.005-T10:00",
+			"section" : "sSTS.005a"
+		},
+		{
+			"room" :    "4-159",
+			"start" :   "11:00",
+			"id" :      "L-STS.011-W11:00",
+			"uri" :     "http://127.0.0.1/L-STS.011-W11%3A00",
+			"end" :     "12:00",
+			"type" :    "Lecture",
+			"day" :     "W",
+			"label" :   "L-STS.011-W11:00",
+			"section" : "sSTS.011a"
+		},
+		{
+			"room" :    "E51-061",
+			"start" :   "11:00",
+			"id" :      "L-STS.435J-M11:00",
+			"uri" :     "http://127.0.0.1/L-STS.435J-M11%3A00",
+			"end" :     "13:00",
+			"type" :    "Lecture",
+			"day" :     "M",
+			"label" :   "L-STS.435J-M11:00",
+			"section" : "sSTS.435Ja"
+		},
+		{
+			"room" :    "1-371",
+			"start" :   "14:30",
+			"id" :      "L-STS.046J-R14:30",
+			"uri" :     "http://127.0.0.1/L-STS.046J-R14%3A30",
+			"end" :     "16:00",
+			"type" :    "Lecture",
+			"day" :     "R",
+			"label" :   "L-STS.046J-R14:30",
+			"section" : "sSTS.046Ja"
+		},
+		{
+			"room" :    "E51-085",
+			"start" :   "13:00",
+			"id" :      "L-STS.260-M13:00",
+			"uri" :     "http://127.0.0.1/L-STS.260-M13%3A00",
+			"end" :     "16:00",
+			"type" :    "Lecture",
+			"day" :     "M",
+			"label" :   "L-STS.260-M13:00",
+			"section" : "sSTS.260a"
+		},
+		{
+			"room" :    "1-150",
+			"start" :   "14:30",
+			"id" :      "L-STS.045-M14:30",
+			"uri" :     "http://127.0.0.1/L-STS.045-M14%3A30",
+			"end" :     "16:00",
+			"type" :    "Lecture",
+			"day" :     "M",
+			"label" :   "L-STS.045-M14:30",
+			"section" : "sSTS.045a"
+		},
+		{
+			"room" :    "66-168",
+			"start" :   "11:00",
+			"id" :      "L-STS.082J-T11:00",
+			"uri" :     "http://127.0.0.1/L-STS.082J-T11%3A00",
+			"end" :     "12:30",
+			"type" :    "Lecture",
+			"day" :     "T",
+			"label" :   "L-STS.082J-T11:00",
+			"section" : "sSTS.082Ja"
+		},
+		{
+			"room" :    "E25-117",
+			"start" :   "16:00",
+			"id" :      "L-STS.482J-R16:00",
+			"uri" :     "http://127.0.0.1/L-STS.482J-R16%3A00",
+			"end" :     "17:30",
+			"type" :    "Lecture",
+			"day" :     "R",
+			"label" :   "L-STS.482J-R16:00",
+			"section" : "sSTS.482Ja"
+		},
+		{
+			"room" :    "E51-385",
+			"start" :   "13:00",
+			"id" :      "L-STS.421-W13:00",
+			"uri" :     "http://127.0.0.1/L-STS.421-W13%3A00",
+			"end" :     "15:00",
+			"type" :    "Lecture",
+			"day" :     "W",
+			"label" :   "L-STS.421-W13:00",
+			"section" : "sSTS.421a"
+		},
+		{
+			"room" :    "E51-385",
+			"start" :   "13:00",
+			"id" :      "L-STS.073-W13:00",
+			"uri" :     "http://127.0.0.1/L-STS.073-W13%3A00",
+			"end" :     "15:00",
+			"type" :    "Lecture",
+			"day" :     "W",
+			"label" :   "L-STS.073-W13:00",
+			"section" : "sSTS.073a"
+		},
+		{
+			"room" :    "1-150",
+			"start" :   "14:30",
+			"id" :      "L-STS.045-W14:30",
+			"uri" :     "http://127.0.0.1/L-STS.045-W14%3A30",
+			"end" :     "16:00",
+			"type" :    "Lecture",
+			"day" :     "W",
+			"label" :   "L-STS.045-W14:30",
+			"section" : "sSTS.045a"
+		},
+		{
+			"room" :    "E51-385",
+			"start" :   "9:00",
+			"id" :      "L-STS.210J-W9:00",
+			"uri" :     "http://127.0.0.1/L-STS.210J-W9%3A00",
+			"end" :     "12:00",
+			"type" :    "Lecture",
+			"day" :     "W",
+			"label" :   "L-STS.210J-W9:00",
+			"section" : "sSTS.210Ja"
+		},
+		{
+			"room" :    "E51-057",
+			"start" :   "9:30",
+			"id" :      "L-STS.029J-R9:30",
+			"uri" :     "http://127.0.0.1/L-STS.029J-R9%3A30",
+			"end" :     "11:00",
+			"type" :    "Lecture",
+			"day" :     "R",
+			"label" :   "L-STS.029J-R9:30",
+			"section" : "sSTS.029Ja"
+		},
+		{
+			"room" :    "HARVARD",
+			"start" :   "18:00",
+			"id" :      "L-STS.449J-T18:00",
+			"uri" :     "http://127.0.0.1/L-STS.449J-T18%3A00",
+			"end" :     "20:00",
+			"type" :    "Lecture",
+			"day" :     "T",
+			"label" :   "L-STS.449J-T18:00",
+			"section" : "sSTS.449Ja"
+		},
+		{
+			"room" :    "E51-061",
+			"start" :   "11:00",
+			"id" :      "L-STS.072J-M11:00",
+			"uri" :     "http://127.0.0.1/L-STS.072J-M11%3A00",
+			"end" :     "13:00",
+			"type" :    "Lecture",
+			"day" :     "M",
+			"label" :   "L-STS.072J-M11:00",
+			"section" : "sSTS.072Ja"
+		},
+		{
+			"room" :    "E51-057",
+			"start" :   "9:30",
+			"id" :      "L-STS.423-R9:30",
+			"uri" :     "http://127.0.0.1/L-STS.423-R9%3A30",
+			"end" :     "11:00",
+			"type" :    "Lecture",
+			"day" :     "R",
+			"label" :   "L-STS.423-R9:30",
+			"section" : "sSTS.423a"
+		},
+		{
+			"room" :    "E51-385",
+			"start" :   "13:00",
+			"id" :      "L-STS.380J-R13:00",
+			"uri" :     "http://127.0.0.1/L-STS.380J-R13%3A00",
+			"end" :     "16:00",
+			"type" :    "Lecture",
+			"day" :     "R",
+			"label" :   "L-STS.380J-R13:00",
+			"section" : "sSTS.380Ja"
+		},
+		{
+			"room" :    "E25-117",
+			"start" :   "16:00",
+			"id" :      "L-STS.482J-T16:00",
+			"uri" :     "http://127.0.0.1/L-STS.482J-T16%3A00",
+			"end" :     "17:30",
+			"type" :    "Lecture",
+			"day" :     "T",
+			"label" :   "L-STS.482J-T16:00",
+			"section" : "sSTS.482Ja"
+		},
+		{
+			"room" :    "16-220",
+			"start" :   "13:00",
+			"id" :      "L-STS.060J-T13:00",
+			"uri" :     "http://127.0.0.1/L-STS.060J-T13%3A00",
+			"end" :     "16:00",
+			"type" :    "Lecture",
+			"day" :     "T",
+			"label" :   "L-STS.060J-T13:00",
+			"section" : "sSTS.060Ja"
+		},
+		{
+			"room" :    "4-159",
+			"start" :   "11:00",
+			"id" :      "L-STS.011-M11:00",
+			"uri" :     "http://127.0.0.1/L-STS.011-M11%3A00",
+			"end" :     "12:00",
+			"type" :    "Lecture",
+			"day" :     "M",
+			"label" :   "L-STS.011-M11:00",
+			"section" : "sSTS.011a"
+		},
+		{
+			"room" :    "66-168",
+			"start" :   "11:00",
+			"id" :      "L-STS.082J-R11:00",
+			"uri" :     "http://127.0.0.1/L-STS.082J-R11%3A00",
+			"end" :     "12:30",
+			"type" :    "Lecture",
+			"day" :     "R",
+			"label" :   "L-STS.082J-R11:00",
+			"section" : "sSTS.082Ja"
+		},
+		{
+			"room" :    "E51-057",
+			"start" :   "9:30",
+			"id" :      "L-STS.029J-T9:30",
+			"uri" :     "http://127.0.0.1/L-STS.029J-T9%3A30",
+			"end" :     "11:00",
+			"type" :    "Lecture",
+			"day" :     "T",
+			"label" :   "L-STS.029J-T9:30",
+			"section" : "sSTS.029Ja"
+		},
+		{
+			"room" :    "4-231",
+			"start" :   "14:00",
+			"id" :      "L-STS.085-R14:00",
+			"uri" :     "http://127.0.0.1/L-STS.085-R14%3A00",
+			"end" :     "17:00",
+			"type" :    "Lecture",
+			"day" :     "R",
+			"label" :   "L-STS.085-R14:00",
+			"section" : "sSTS.085a"
+		},
+		{
+			"room" :    "E51-057",
+			"start" :   "19:00",
+			"id" :      "L-STS.051-T19:00",
+			"uri" :     "http://127.0.0.1/L-STS.051-T19%3A00",
+			"end" :     "10:00",
+			"type" :    "Lecture",
+			"day" :     "T",
+			"label" :   "L-STS.051-T19:00",
+			"section" : "sSTS.051a"
+		},
+		{
+			"room" :    "54-100",
+			"start" :   "10:00",
+			"id" :      "L-STS.005-R10:00",
+			"uri" :     "http://127.0.0.1/L-STS.005-R10%3A00",
+			"end" :     "11:00",
+			"type" :    "Lecture",
+			"day" :     "R",
+			"label" :   "L-STS.005-R10:00",
+			"section" : "sSTS.005a"
+		},
+		{
+			"room" :    "E51-385",
+			"start" :   "12:00",
+			"id" :      "L-STS.250J-T12:00",
+			"uri" :     "http://127.0.0.1/L-STS.250J-T12%3A00",
+			"end" :     "15:00",
+			"type" :    "Lecture",
+			"day" :     "T",
+			"label" :   "L-STS.250J-T12:00",
+			"section" : "sSTS.250Ja"
+		},
+		{
+			"room" :    "E51-057",
+			"start" :   "9:30",
+			"id" :      "L-STS.423-T9:30",
+			"uri" :     "http://127.0.0.1/L-STS.423-T9%3A30",
+			"end" :     "11:00",
+			"type" :    "Lecture",
+			"day" :     "T",
+			"label" :   "L-STS.423-T9:30",
+			"section" : "sSTS.423a"
+		}
+	],
+	"types" :      {
+		"Lecture" : {
+			"uri" : "http://127.0.0.1/Lecture"
+		}
+	},
+	"properties" : {
+		"end" :     {
+			"uri" : "http://127.0.0.1/end"
+		},
+		"day" :     {
+			"uri" : "http://127.0.0.1/day"
+		},
+		"section" : {
+			"uri" : "http://127.0.0.1/section"
+		},
+		"room" :    {
+			"uri" : "http://127.0.0.1/room"
+		},
+		"start" :   {
+			"uri" : "http://127.0.0.1/start"
+		},
+		"id" :      {
+			"uri" : "http://simile.mit.edu/2006/11/exhibit#id"
+		}
+	}
+}
---
+++ src/webapp/data/sts-sections.js
@@ -0,0 +1,214 @@
+{
+	"items" :      [
+		{
+			"instructor" : "Jones",
+			"label" :      "sSTS.005a",
+			"type" :       "Section",
+			"uri" :        "http://127.0.0.1/sSTS.005a",
+			"class" :      "cSTS.005",
+			"id" :         "sSTS.005a"
+		},
+		{
+			"instructor" : "Fischer",
+			"label" :      "sSTS.250Ja",
+			"type" :       "Section",
+			"uri" :        "http://127.0.0.1/sSTS.250Ja",
+			"class" :      "cSTS.250J",
+			"id" :         "sSTS.250Ja"
+		},
+		{
+			"instructor" : "Schull",
+			"label" :      "sSTS.045a",
+			"type" :       "Section",
+			"uri" :        "http://127.0.0.1/sSTS.045a",
+			"class" :      "cSTS.045",
+			"id" :         "sSTS.045a"
+		},
+		{
+			"instructor" : "Helmreich",
+			"label" :      "sSTS.060Ja",
+			"type" :       "Section",
+			"uri" :        "http://127.0.0.1/sSTS.060Ja",
+			"class" :      "cSTS.060J",
+			"id" :         "sSTS.060Ja"
+		},
+		{
+			"instructor" : "Sur",
+			"label" :      "sSTS.046Ja",
+			"type" :       "Section",
+			"uri" :        "http://127.0.0.1/sSTS.046Ja",
+			"class" :      "cSTS.046J",
+			"id" :         "sSTS.046Ja"
+		},
+		{
+			"instructor" : "Oye",
+			"label" :      "sSTS.482Ja",
+			"type" :       "Section",
+			"uri" :        "http://127.0.0.1/sSTS.482Ja",
+			"class" :      "cSTS.482J",
+			"id" :         "sSTS.482Ja"
+		},
+		{
+			"instructor" : "Smith",
+			"label" :      "sSTS.423a",
+			"type" :       "Section",
+			"uri" :        "http://127.0.0.1/sSTS.423a",
+			"class" :      "cSTS.423",
+			"id" :         "sSTS.423a"
+		},
+		{
+			"instructor" : [
+				"Fischer",
+				"Abelson"
+			],
+			"label" :      "sSTS.085a",
+			"type" :       "Section",
+			"uri" :        "http://127.0.0.1/sSTS.085a",
+			"class" :      "cSTS.085",
+			"id" :         "sSTS.085a"
+		},
+		{
+			"instructor" : "Perdue",
+			"label" :      "sSTS.210Ja",
+			"type" :       "Section",
+			"uri" :        "http://127.0.0.1/sSTS.210Ja",
+			"class" :      "cSTS.210J",
+			"id" :         "sSTS.210Ja"
+		},
+		{
+			"instructor" : [
+				"Lepinay",
+				"Mindell"
+			],
+			"label" :      "sSTS.260a",
+			"type" :       "Section",
+			"uri" :        "http://127.0.0.1/sSTS.260a",
+			"class" :      "cSTS.260",
+			"id" :         "sSTS.260a"
+		},
+		{
+			"instructor" : "Foley",
+			"label" :      "sSTS.011a",
+			"type" :       "Section",
+			"uri" :        "http://127.0.0.1/sSTS.011a",
+			"class" :      "cSTS.011",
+			"id" :         "sSTS.011a"
+		},
+		{
+			"instructor" : "Postol",
+			"label" :      "sSTS.072Ja",
+			"type" :       "Section",
+			"uri" :        "http://127.0.0.1/sSTS.072Ja",
+			"class" :      "cSTS.072J",
+			"id" :         "sSTS.072Ja"
+		},
+		{
+			"instructor" : [
+				"Silbey",
+				"McCants"
+			],
+			"label" :      "sSTS.380Ja",
+			"type" :       "Section",
+			"uri" :        "http://127.0.0.1/sSTS.380Ja",
+			"class" :      "cSTS.380J",
+			"id" :         "sSTS.380Ja"
+		},
+		{
+			"instructor" : "Oye",
+			"label" :      "sSTS.082Ja",
+			"type" :       "Section",
+			"uri" :        "http://127.0.0.1/sSTS.082Ja",
+			"class" :      "cSTS.082J",
+			"id" :         "sSTS.082Ja"
+		},
+		{
+			"instructor" : "HARVARD",
+			"label" :      "sSTS.449Ja",
+			"type" :       "Section",
+			"uri" :        "http://127.0.0.1/sSTS.449Ja",
+			"class" :      "cSTS.449J",
+			"id" :         "sSTS.449Ja"
+		},
+		{
+			"instructor" : "Smith",
+			"label" :      "sSTS.029Ja",
+			"type" :       "Section",
+			"uri" :        "http://127.0.0.1/sSTS.029Ja",
+			"class" :      "cSTS.029J",
+			"id" :         "sSTS.029Ja"
+		},
+		{
+			"instructor" : "Lepinay",
+			"label" :      "sSTS.051a",
+			"type" :       "Section",
+			"uri" :        "http://127.0.0.1/sSTS.051a",
+			"class" :      "cSTS.051",
+			"id" :         "sSTS.051a"
+		},
+		{
+			"instructor" : "Postol",
+			"label" :      "sSTS.435Ja",
+			"type" :       "Section",
+			"uri" :        "http://127.0.0.1/sSTS.435Ja",
+			"class" :      "cSTS.435J",
+			"id" :         "sSTS.435Ja"
+		},
+		{
+			"instructor" : [
+				"Fischer",
+				"Abelson"
+			],
+			"label" :      "sSTS.487a",
+			"type" :       "Section",
+			"uri" :        "http://127.0.0.1/sSTS.487a",
+			"class" :      "cSTS.487",
+			"id" :         "sSTS.487a"
+		},
+		{
+			"instructor" : [
+				"Postol",
+				"Ghoshroy"
+			],
+			"label" :      "sSTS.421a",
+			"type" :       "Section",
+			"uri" :        "http://127.0.0.1/sSTS.421a",
+			"class" :      "cSTS.421",
+			"id" :         "sSTS.421a"
+		},
+		{
+			"instructor" : [
+				"Postol",
+				"Ghoshroy"
+			],
+			"label" :      "sSTS.073a",
+			"type" :       "Section",
+			"uri" :        "http://127.0.0.1/sSTS.073a",
+			"class" :      "cSTS.073",
+			"id" :         "sSTS.073a"
+		},
+		{
+			"instructor" : "Resnick",
+			"label" :      "sSTS.445Ja",
+			"type" :       "Section",
+			"uri" :        "http://127.0.0.1/sSTS.445Ja",
+			"class" :      "cSTS.445J",
+			"id" :         "sSTS.445Ja"
+		}
+	],
+	"types" :      {
+		"Section" : {
+			"uri" : "http://127.0.0.1/Section"
+		}
+	},
+	"properties" : {
+		"instructor" : {
+			"uri" : "http://127.0.0.1/instructor"
+		},
+		"class" :      {
+			"uri" : "http://127.0.0.1/class"
+		},
+		"id" :         {
+			"uri" : "http://simile.mit.edu/2006/11/exhibit#id"
+		}
+	}
+}
