f816ae59354bee45c8b3bf272de5d32454e3b50d
lizs <lizs>
2011-07-20 19:35:51 +0000

    Removed "load all HASS-D" button from index.html; Moved "favorites" section in browse.js to favorites.js.

--- src/webapp/scripts/browse.js
+++ src/webapp/scripts/browse.js
@@ -66,7 +66,9 @@ function onLoad() {
         }
     }
     urls.push("data/schema.js");
+    /** Include if using image facet
     urls.push("data/courses.json");
+    **/
 
 
     // pull necessary URLs from cookie, since window.database doesn't exist yet
@@ -110,31 +112,10 @@ function onLoad() {
 		};
         pickedSections._update();
         pickedClasses._update();
-        
-        /**
-        if (hasTQE) {
-            var tqeDiv = document.getElementById("tqe-facet");
-            tqeDiv.onclick = function() { makeFacet(this); };
-            tqeDiv.setAttribute("ex:role", "facet");
-            tqeDiv.setAttribute("ex:expression", ".TQE");
-            tqeDiv.setAttribute("ex:facetLabel", "TQE &raquo;");
-        }
-        */
-        
         window.exhibit = Exhibit.create();
         window.exhibit.setCollection("picked-sections", pickedSections);
         window.exhibit.setCollection("picked-classes", pickedClasses);
         window.exhibit.configureFromDOM();
-        
-        /*
-        setupExistingFacet(document.getElementById("category-facet"));
-        setupExistingFacet(document.getElementById("semester-facet"));
-        setupExistingFacet(document.getElementById("offering-facet"));
-        if (hasTQE) {
-            setupExistingFacet(document.getElementById("tqe-facet"));
-        }
-        */
-        
         enableMiniTimegrid();
         enableUnitAdder();
         fillAddMoreSelect();
@@ -555,74 +536,7 @@ function unmakeFacet(div) {
  * Favorites
  *==================================================
  */
-function toggleFavorite(img) {
-    var classID = img.getAttribute("classID");
-    var favorite = window.database.getObject(classID, "favorite") == "true";
-    if (favorite) {
-        SimileAjax.History.addLengthyAction(
-            function() { undoFavorite(classID, img) },
-            function() { doFavorite(classID, img) },
-            "Unfavorite " + classID
-        );
-        
-    } else {
-        SimileAjax.History.addLengthyAction(
-            function() { doFavorite(classID, img) },
-            function() { undoFavorite(classID, img) },
-            "Favorite " + classID
-        );
-    }
-}
 
-function doFavorite(classID, img) {
-    window.database.addStatement(classID, "favorite", "true");
-    img.src = "images/yellow-star.png";
-    possiblyLog({
-        "picker-mark-favorite":classID
-    });
-}
-
-function undoFavorite(classID, img) {
-    window.database.removeStatement(classID, "favorite", "true");
-    img.src = "images/gray-star.png";
-    possiblyLog({
-        "picker-remove-favorite":classID
-    });
-}
-
-function processShowOnlyFavoriteClass(checkbox) {
-    if (checkbox.checked) {
-        SimileAjax.History.addLengthyAction(
-            function() { showFavoritesOnly(); checkbox.checked = true; },
-            function() { showAllClasses(); checkbox.checked = false; },
-            "Show favorites only"
-        );
-    } else {
-        SimileAjax.History.addLengthyAction(
-            function() { showAllClasses(); checkbox.checked = false; },
-            function() { showFavoritesOnly(); checkbox.checked = true; },
-            "Show all classes"
-        );
-    }
-};
-
-function showFavoritesOnly() {
-    var collection = window.exhibit.getDefaultCollection();
-    collection._items = window.database.getSubjects("true", "favorite");
-    collection._onRootItemsChanged();
-    possiblyLog({
-        "picker-toggle-favorite-filter":"show-favorites-only"
-    });
-}
-
-function showAllClasses() {
-    var collection = window.exhibit.getDefaultCollection();
-    collection._items = window.database.getSubjects("Class", "type");
-    collection._onRootItemsChanged();
-    possiblyLog({
-        "picker-toggle-favorite-filter":"show-all"
-    });
-}
 
 /*==================================================
  * Section picking
