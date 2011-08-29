ae84c7faa89ae3e97e8e9f58521788e4928803f7
lizs <lizs>
2011-08-29 22:06:00 +0000

    In processOfficialDataItem, created code for new coursews data. Changed loadStaticData to a jQuery get.

--- src/webapp/scripts/load-and-process.js
+++ src/webapp/scripts/load-and-process.js
@@ -5,10 +5,10 @@ function loadURLs(urls, fDone) {
     var fNext = function() {
         if (urls.length > 0) {
             var url = urls.shift();
+
             if (url.search(/http/) == 0) {
                 // For coursews data
-            	Exhibit.importers["application/jsonp"].load(
-                    url, window.database, fNext, processOfficialData);
+            	Exhibit.importers["application/jsonp"].load(url, window.database, fNext, processOfficialData);
             } else {
             	loadStaticData(url, window.database, fNext);
             }
@@ -20,33 +20,46 @@ function loadURLs(urls, fDone) {
 }
 
 function processOfficialData(json) {
-    var items = json.items;				
+    var items = json.items;			
     for (var i = 0; i < items.length; i++) {
         processOfficialDataItem(items[i]);
-    }					
+    }			
     return json;
 }
 
 function processOfficialDataItem(item) {
-
     if ('prereqs' in item) {
         item.prereqs = processPrereqs(item.prereqs);
 	}
 
+    for (attribute in item) {
+        if (item[attribute] == '') {
+            delete item[attribute];
+        }
+    }
+    if ('master_subject_id' in item) {
+        item.id = item.master_subject_id;
+    }
+
+    if (term == 'iap') { }
+    else if (term == 'fall') {
+        item.Instructor = item.fall_instructors;
+    } 
+    else {
+        item.Instructor = item.spring_instructors;
+    }
+    if ('equivalent_subjects' in item) {
+        item.equivalent_subjects = courseArrayToLinks(item.equivalent_subjects);
+    }
+
 	if ('timeAndPlace' in item) {
-		if (typeof item.timeAndPlace != "string") {
-			item.timeAndPlace = item.timeAndPlace.join(", ");
-		} 
 		if (item.timeAndPlace.search(/ARRANGED/) >= 0 || item.timeAndPlace.search(/null/) >= 0) {
 			item.timeAndPlace = 'To be arranged';
 		}
 	}
-
-	if ('units' in item) {
-		if (item.units == '0-0-0' || item.units == 'unknown') {
-			item.units = 'Arranged';
-			item['total-units'] = 'Arranged';
-		}
+	if ('units' in item && item.is_variable_units == 'Y') {
+		item['units'] = 'Arranged';
+		item['total-units'] = 'Arranged';
 	}
 
     if ('offering' in item) {
@@ -75,61 +88,65 @@ function processPrereqs(prereqs) {
     if (prereqs == "") {
 		prereqs = "--";
 	}
-	while (prereqs.search(/GIR:/) >= 0) {
-		gir = prereqs.match(/GIR:.{4}/);
-		prereqs = prereqs.replace(/GIR:.{4}/, girData[gir].join(" or "));
-	}
-	while (prereqs.search(/[\]\[]/) >= 0 ) {
-		prereqs = prereqs.replace(/[\]\[]/, "");
-	}
-
-    // Makes prereqs appear as links
-	var matches = prereqs.match(/([^\s\/]+\.[\d]+\w?)/g);
-	if (matches != null) {
-		var s = prereqs;
-		var output = "";
-		var from = 0;
-		for (var m = 0; m < matches.length; m++) {
-			var match = matches[m];
-			var i = s.indexOf(match, from);
-			var replace = 
-				"<a href=\"javascript:{}\" onclick=\"showPrereq(this, '" +
-					match.replace(/J/, "")+"');\">" + match + "</a>";
-			
-			output += s.substring(from, i) + replace;
-			from = i + match.length;
-		}
-		prereqs = output + s.substring(from);
+    else {
+	    while (prereqs.search(/GIR:/) >= 0) {
+		    gir = prereqs.match(/GIR:.{4}/);
+		    prereqs = prereqs.replace(/GIR:.{4}/, girData[gir].join(" or "));
+	    }
+	    while (prereqs.search(/[\]\[]/) >= 0 ) {
+		    prereqs = prereqs.replace(/[\]\[]/, "");
+	    }
     }
-    
+    // Makes prereqs appear as links
+    prereqs = coursesToLinks(prereqs);
     return prereqs;
 }
 
+function courseArrayToLinks(array) {
+    string = array.join(',, ');
+    string = coursesToLinks(string);
+    return string.split(',, ');
+}
+
+function coursesToLinks(courseString) {
+    // Any number of spaces followed by any number of digits
+    // followed by, optionally, a letter
+    var courseArray = courseString.match(/([^\s\/]+\.[\d]+\w?)/g);
+    if (courseArray != null) {
+        var string = courseString;
+        var output = '';
+        var upTo = 0;
+        for (var c=0; c<courseArray.length; c++) {
+            var course = courseArray[c];
+            var index = string.indexOf(course, upTo);
+            var replacement = '<a href=\'javascript:{}\' onclick=\'showPrereq(this, "' + course.replace(/J/, '')+'");\'>' + course + '</a>';
+            // string.substring(upTo, index) is everything not being replaced
+            output += string.substring(upTo, index) + replacement;
+            upTo = index + course.length;
+        }
+        courseString = output + string.substring(upTo);
+    }
+    return courseString;
+}
+
+// http://api.jquery.com/jQuery.ajax/
 function loadStaticData(link, database, cont) {
     var url = typeof link == "string" ? link : link.href;
     // Documentation: simile-widgets.org/wiki/Exhibit/API/2.2.0/Persistence
     // Given a relative or absolute URL, returns the absolute URL
     url = Exhibit.Persistence.resolveURL(url);
 
-    var fError = function(statusText, status, xmlhttp) {
+    var fError = function(jqXHR, textStatus, errorThrown) {
         Exhibit.UI.hideBusyIndicator();
         Exhibit.UI.showHelp(Exhibit.l10n.failedToLoadDataFileMessage(url));
         if (cont) cont();
     };
     
-    var fDone = function(xmlhttp) {
+    var fSuccess = function(jsonObject, textStatus, jqXHR) {
         Exhibit.UI.hideBusyIndicator();
         try {
-            var JSONobject = null;
-            try {
-                // xmlhttp.responseText is JSON data
-                JSONobject = eval("(" + xmlhttp.responseText + ")");
-            } catch (e) {
-                Exhibit.UI.showJsonFileValidation(Exhibit.l10n.badJsonMessage(url, e), url);
-            }
-            
-            if (JSONobject != null) {
-                database.loadData(JSONobject, Exhibit.Persistence.getBaseURL(url));
+            if (jsonObject != null) {
+                database.loadData(jsonObject, Exhibit.Persistence.getBaseURL(url));
             }
         } catch (error) {
             SimileAjax.Debug.exception(error, "Error loading Exhibit JSON data from " + url);
@@ -139,8 +156,9 @@ function loadStaticData(link, database, cont) {
     };
 
     Exhibit.UI.showBusyIndicator();
-    // can be replaced by jQuery
-    // performs an asynchronous HTTP get
-    // Calls fDone(url) if url in right form, otherwise calls fError
-    SimileAjax.XmlHttp.get(url, fError, fDone);
+    // Calls fSuccess if data is json in correct form, else calls fError
+    $.ajax({ url : url,
+        error : fError,
+        success: fSuccess,
+        dataType: 'json' });
 }
