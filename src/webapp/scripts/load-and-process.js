f419c451bcc52a283f699787960bf5dda9899992
lizs <lizs>
2012-01-13 20:10:07 +0000

    If a class is a cross-listed class and the "master class" does not lie in one of the loaded courses, load the master class singly from one of the unloaded courses. Significant changes.

--- src/webapp/scripts/load-and-process.js
+++ src/webapp/scripts/load-and-process.js
@@ -1,14 +1,21 @@
 // Formerly part of browse.js
 // Look there for svn history
 
-function loadURLs(urls, fDone) {
+function loadURLs(urls, fDone, singleClass) {
+//function loadURLs(urls, fDone) {
     var fNext = function() {
         if (urls.length > 0) {
             var url = urls.shift();
 
             if (url.search(/http/) == 0) {
                 // For coursews data
-            	Exhibit.importers["application/jsonp"].load(url, window.database, fNext, processOfficialData);
+            	Exhibit.importers["application/jsonp"].load(
+                        url, 
+                        window.database, 
+                        fNext, 
+                        function(json) {
+                            return processOfficialData(json, singleClass);
+                        });
             } else {
             	loadStaticData(url, window.database, fNext);
             }
@@ -19,15 +26,53 @@ function loadURLs(urls, fDone) {
     fNext();
 }
 
-function processOfficialData(json) {
-    var items = json.items;			
-    for (var i = 0; i < items.length; i++) {
-        processOfficialDataItem(items[i]);
-    }			
+function processOfficialData(json, singleClass) {
+    var items = json.items;
+
+    // If singleClass exists, we are here loading only the one class
+    // that is the "master class" of a cross-listed class
+    if (singleClass) {
+        var classes = [];
+        for (var i=0; i < items.length; i++) {
+            var item = items[i];
+            if (singleClass == item.id || singleClass == item['section-of']) {
+                classes.push(item);
+            }
+        }
+
+        for (var j = 0; j < classes.length; j++) {
+            processOfficialDataItem(classes[j]);
+        }
+        json.items = classes;
+    }	
+    
+    // Otherwise, we are loading all the classes in a course
+    else {
+        for (var i = 0; i < items.length; i++) {
+            var item = items[i];
+            // If true this is a cross-listed class, and this is not the "master" class
+            // Ex. if this class is 18.062, rename it 6.042 and load 6.042
+            if ('master_subject_id' in item && item.id != item.master_subject_id) {
+                var course = item.master_subject_id.split('.')[0];
+                if (!isLoaded(course)) {
+                    loadSingleClass(item.master_subject_id);
+                }
+                // Once both 18.062 and 6.042 are loaded, they will be merged as one class with two courses ["6", "18"]
+                items[i] = {"id":item.master_subject_id, "course":item.course, "label":item.label, "type":item.type};
+            }
+            // Otherwise, this is a regular class -- just load it
+            else {
+                processOfficialDataItem(item);
+            }
+        }
+    }
     return json;
 }
 
+
+
 function processOfficialDataItem(item) {
+
     if ('prereqs' in item) {
         item.prereqs = processPrereqs(item.prereqs);
 	}
@@ -37,12 +82,9 @@ function processOfficialDataItem(item) {
             delete item[attribute];
         }
     }
-    if ('master_subject_id' in item) {
-        item.id = item.master_subject_id;
-    }
 
     if (term == 'iap') { }
-    else if (term == 'fall') {
+    else if (term == 'FA') {
         item.Instructor = item.fall_instructors;
     } 
     else {
@@ -80,7 +122,7 @@ function processOfficialDataItem(item) {
         item.type = 'LabSection';
         item["lab-section-of"] = item["section-of"];
         delete item["section-of"];
-    } 
+    }
 }
 
 // Puts a string of prereqs into correct format
