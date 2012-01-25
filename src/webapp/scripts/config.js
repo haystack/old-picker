00a2fb2d673460a59aa460480db318e22157dc5c
lizs <lizs>
2012-01-25 01:20:15 +0000

    Added new courses and changed the information that courses hold.

--- src/webapp/scripts/config.js
+++ src/webapp/scripts/config.js
@@ -2,210 +2,143 @@ var term = "SP";
 
 var courses = [
     {   number: "1",
-        name:   "Civil and Environmental Engineering",
-        hasData: true,
-        //hassd: true,
+        name:   "Civil and Environmental Engineering"
     },
     {   number:  "2",
-        name:    "Mechanical Engineering",
-       // hassd: false,
-        hasData: true
+        name:    "Mechanical Engineering"
     },
     {   number:  "3",
-        name:    "Materials Science and Engineering",
-      //  hassd: true,
-        hasData: true
+        name:    "Materials Science and Engineering"
     },
     {   number: "4",
-        name:   "Architecture",
-    //    hassd: true,
-        hasData: true
+        name:   "Architecture"
     },
     {   number: "5",
-        name:   "Chemistry",
-   //     hassd: false,
-        hasData: true
+        name:   "Chemistry"
     },
     {   number: "6",
-        name:   "Electrical Engineering and Computer Science",
-     //   hassd: false,
-        hasData: true
+        name:   "Electrical Engineering and Computer Science"
+    },
+    {   number: "6-7",
+        name: "Computer Science and Molecular Biology",
+        interdepartmental: true
     },
     {   number: "7",
-        name:   "Biology",
-       // hassd: false,
-        hasData: true
+        name:   "Biology"
     },
     {   number:  "8",
-        name:    "Physics",
- //       hassd: false,
-        hasData: true
+        name:    "Physics"
     },
     {   number: "9",
-        name:   "Brain and Cognitive Sciences",
-   //     hassd: true,
-        hasData: true
+        name:   "Brain and Cognitive Sciences"
     },
     {   number: "10",
-        name:   "Chemical Engineering",
-     //   hassd: false,
-        hasData: true
+        name:   "Chemical Engineering"
     },
     {   number:  "11",
-        name:    "Urban Studies and Planning",
-       // hassd: true,
-        hasData: true
+        name:    "Urban Studies and Planning"
     },
     {   number: "12",
-        name:   "Earth, Atmospheric, and Planetary Sciences",
-  //      hassd: false,
-        hasData: true
+        name:   "Earth, Atmospheric, and Planetary Sciences"
     },
     {   number: "13",
-        name: "Ocean Engineering",
-        hasData: true
+        name: "Ocean Engineering"
     },
     {   number: "14",
-        name:   "Economics",
-    //    hassd: true,
-        hasData: true
+        name:   "Economics"
     },
     {   number: "15",
-        name:   "Business (see Sloan School of Management)",
-      //  hassd: false,
-        hasData: true
+        name:   "Business (see Sloan School of Management)"
     },
     {   number: "16",
-        name:   "Aeronautics and Astronautics",
-        //hassd: false,
-        hasData: true
+        name:   "Aeronautics and Astronautics"
     },
     {    number:    "17",
-        name:    "Political Science",
-   //     hassd: true,
-        hasData: true
+        name:    "Political Science"
     },
     {    number:    "18",
-        name:    "Mathematics",
-     //   hassd: false,
-        hasData: true
+        name:    "Mathematics"
+    },
+    {   number: "18C",
+        name: "Mathematics with Computer Science",
+        interdepartmental: true
     },
     {   number: "20",
-        name:   "Biological Engineering",
-       // hassd: false,
-        hasData: true
+        name:   "Biological Engineering"
     },
     {   number: "21A",
         name:   "Anthropology",
-  //      hassd: true,
-        hasData: true
     },
     {   number: "21F",
-        name:   "Foreign Languages and Literatures",
-    //    hassd: true,
-        hasData: true
+        name:   "Foreign Languages and Literatures"
     },
     {   number: "21H",
-        name:   "History",
-      //  hassd: true,
-        hasData: true
+        name:   "History"
     },
     {    number:    "21L",
-        name:    "Literature",
-        //hassd: true,
-        hasData: true
+        name:    "Literature"
     },
     {    number:    "21M",
-        name:    "Music and Theater Arts",
-   //     hassd: true,
-        hasData: true
+        name:    "Music and Theater Arts"
     },
     {    number:    "21W",
-        name:    "Writing and Humanistic Studies",
-     //   hassd: true,
-        hasData: true
-    },
-    {   number: "WGS",
-        name: "Women's and Gender Studies",
-        hasData: true
+        name:    "Writing and Humanistic Studies"
     },
     {    number:    "22",
-        name:    "Nuclear Science and Engineering",
-        hasData: true
+        name:    "Nuclear Science and Engineering"
     },
     {   number: "24",
-        name:   "Linguistics and Philosophy",
-       // hassd: true,
-        hasData: true
+        name:   "Linguistics and Philosophy"
     },
     {    number:    "CC",
-        name:    "Concourse",
-        hasData: true
+        name:    "Concourse"
     },
     {   number: "CMS",
-        name:   "Comparative Media Studies",
- //       hassd: true,
-        hasData: true
+        name:   "Comparative Media Studies"
     },
     {    number:    "CSB",
-        name:    "Computational and Systems Biology",
-        hasData: true
+        name:    "Computational and Systems Biology"
     },
     {    number:    "EC",
-        name:    "Edgerton Center",
-        hasData: true
+        name:    "Edgerton Center"
     },
     {    number:    "ES",
-        name:    "Experimental Study Group",
-        hasData: true
+        name:    "Experimental Study Group"
     },
     {   number: "ESD",
-        name:   "Engineering Systems Division",
-   //     hassd: false,
-        hasData: true
+        name:   "Engineering Systems Division"
     },
     {   number: "HST",
-        name:   "Health Sciences and Technology",
-     //   hassd: false,
-        hasData: true
+        name:   "Health Sciences and Technology"
     },
     {   number:  "MAS",
-        name:    "Media Arts and Sciences (Media Lab)",
-       // hassd: true,
-        hasData: true
+        name:    "Media Arts and Sciences (Media Lab)"
+    },
+    {   number: "OR",
+        name: "Operations Research",
+        interdepartmental: true
     },
     {    number:    "AS",
-        name:    "ROTC - Aerospace Studies",
-        hasData: true
+        name:    "ROTC - Aerospace Studies"
     },
     {    number:    "MS",
-        name:    "ROTC - Military Science",
-        hasData: true
+        name:    "ROTC - Military Science"
     },
     {    number:    "NS",
-        name:    "ROTC - Naval Science",
-        hasData: true
+        name:    "ROTC - Naval Science"
     },
     {   number:  "STS",
-        name:    "Science, Technology, and Society",
-  //      hassd: true,
-        hasData: true
+        name:    "Science, Technology, and Society"
     },
     {    number:    "SWE",
-        name:    "Engineering School-Wide Electives",
-        hasData: true
+        name:    "Engineering School-Wide Electives"
     },
     {   number:  "SP",
-	    name:	 "Special Programs",
-    //    hassd: true,
-	    hasData:  true
+	    name:	 "Special Programs"
     },
-/*
-    {   number:  "hass_d",
-        name:    "HASS D",
-        hasData: true
-    }
-*/
+    {   number: "WGS",
+        name: "Women's and Gender Studies"
+    },
 ];
 
 var colorTable = [
@@ -297,46 +230,3 @@ var girData = {
 	"GIR:BIOL": ["7.012", "7.013", "7.014"],
 	"GIR:CHEM": ["3.091", "5.111", "5.112"]
 }
-
-/* PersistentData object: stores functionality to deal with persistent
- * data in a cleaner way.
- */
-/*
-var PersistentData = {};
-
-PersistentData.readCookie = function(name) {
-    var start = document.cookie.indexOf(name + '=');
-    if (start != -1) {
-        start = start + name.length + 1;
-        var end = document.cookie.indexOf(';', start);
-        if (end == -1)
-            end = document.cookie.length;
-        var content = unescape(document.cookie.substring(start, end));
-        if (content != 'null')
-        	return content;
-        return '';
-    }
-    return '';
-}
-*/
-/**
- * Returns an Exhibit.Set of the requested stored data
- */
-/*
-PersistentData.stored = function(name) {
-    var sections;
-    
-	// if Exhibit loaded MySQL data re: picked sections
-	if (window.database &&
-		window.database.getObjects(name, 'list').size() > 0) {
-		sections = window.database.getObjects(name, 'list');
-	}
-	else { // no prior user data exists, check cookie
-		stringArr = this.readCookie(name);
-		elts = stringArr.split(',');
-		sections = new Exhibit.Set(elts);
-	}
-
-	return sections;
-}
-*/
